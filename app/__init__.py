7fe8a3fdaaa65a62b6dfcbb99755b5f01f8022ce
Brad <bradley.thompson@mymona.uwi.edu>
2019-04-01 03:15:58 +0000

    created upload path and did bonus question

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,16 @@
 from flask import Flask
+from flask_wtf.csrf import CSRFProtect
+
+
+UPLOAD_FOLDER = './app/static/uploads'
+ALLOWED_IMAGES = set(['jpg', 'png', 'JPG','jpeg'])
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'v\xf9\xf7\x11\x13\x18\xfaMYp\xed_\xe8\xc9w\x06\x8e\xf0f\xd2\xba\xfd\x8c\xda'
+csrf = CSRFProtect(app)
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['ALLOWED_IMAGES'] = ALLOWED_IMAGES
+
 
 from app import views
--- app/static/js/app.js
+++ app/static/js/app.js
@@ -12,6 +12,9 @@ Vue.component('app-header', {
           <li class="nav-item active">
             <router-link class="nav-link" to="/">Home <span class="sr-only">(current)</span></router-link>
           </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/upload">Upload</router-link>
+          </li>
         </ul>
       </div>
     </nav>
@@ -40,6 +43,102 @@ const Home = Vue.component('home', {
     }
 });
 
+const uploadForm = Vue.component('upload-form',{
+    template: ` 
+    <div class="upload">
+        <h2>Upload Form</h2>
+        
+        <div v-if='messageFlag' >
+        
+            <div v-if="!errorFlag ">
+                <div class="alert alert-success" >
+                    <p>File Upload Successful</p>                
+                </div>
+            </div>
+            
+            
+            <div v-else >
+                <ul class="alert alert-danger">
+                    <li v-for="error in errorss">
+                        {{ error[0] }}<br>
+                    </li>
+                    <li v-for="error in errorss">
+                        {{ error[1] }}
+                    </li>
+                </ul>
+            </div>
+            
+        </div>
+        
+        <div class="form-inline d-flex justify-content">
+            <form id="uploadForm"  @submit.prevent="uploadPhoto" method="POST" enctype="multipart/form-data">
+                <div>
+                <div for="msg">Description </div>
+                <textarea class="form-control" rows="2"  cols="95" id="msg" name="description"></textarea><br></br>
+                <div for="pic">Photo Upload </div>
+                
+                <input type="file" name="photo" id="upload"/> </br>
+                </div></br>
+                <button class="btn btn-success" type="submit">Submit</button>
+            </form>
+        </div>
+    </div>
+    `,
+    methods: {
+    uploadPhoto: function(){
+        
+        let self = this;
+        let uploadForm = document.getElementById('uploadForm');
+        let form_data = new FormData(uploadForm);
+        
+        fetch("/api/upload", {
+            method: 'POST',
+            body: form_data,
+            headers: {
+                'X-CSRFToken': token
+            },
+            credentials: 'same-origin'
+        })
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (jsonResponse) {
+            // display a success message
+             self.messageFlag = true;
+
+                if (jsonResponse.hasOwnProperty("errors")){
+                    self.errorFlag=true;
+                    self.errorss = jsonResponse.errors;
+                }
+                else if(jsonResponse.hasOwnProperty("message")){
+                    self.errorFlag = false;
+                    self.response = jsonResponse.message;
+                    
+                }
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+    },
+       onFileSelected: function(){
+            let self = this;
+            let filenameArr = $("#photo")[0].value.split("\\");
+            self.filename = filenameArr[filenameArr.length-1];
+        }
+    },
+    data: function(){
+        return {
+            errorFlag: false,
+            messageFlag: false,
+            errorss: [],
+            response:[],
+            filename: ""
+        };
+    }
+});
+
+
+
 const NotFound = Vue.component('not-found', {
     template: `
     <div>
@@ -57,7 +156,7 @@ const router = new VueRouter({
     routes: [
         {path: "/", component: Home},
         // Put other routes here
-
+        { path: "/upload", component: uploadForm },
         // This is a catch all route in case none of the above matches
         {path: "*", component: NotFound}
     ]
